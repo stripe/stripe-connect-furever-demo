a88163ac40562870d8ea65953261423892171ab5
slye-stripe <104949269+slye-stripe@users.noreply.github.com>
2023-03-10 11:37:21 -0800

    Add check for href attribute (#26)

--- server/public/javascripts/persistQueryParams.js
+++ server/public/javascripts/persistQueryParams.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('a').forEach((el) => {
     const href = el.getAttribute('href');
     // Only if it's a relative path
-    if (href.startsWith('/')) {
+    if (href && href.startsWith('/')) {
       el.setAttribute('href', href + window.location.search);
     }
   });
