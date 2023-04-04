34f1c790022ec7316e5cc75bd87b56eebcd8d516
Samuel Lye <slye@stripe.com>
2023-04-04 11:23:21 -0700

    Remove test payments and payouts if requirements.disabled_reason is truthy

--- server/app.js
+++ server/app.js
@@ -104,6 +104,7 @@ app.use(async (req, res, next) => {
     // Get the stripe account object if available
     if (req.user.stripeAccountId) {
       req.stripeAccount = await retrieveStripeAccount(req.user.stripeAccountId);
+      res.locals.disabled = !!req.stripeAccount?.requirements?.disabled_reason;
       // The onboardingComplete flag is used to know if the account has completed onboarding
       res.locals.onboardingComplete = !!req.stripeAccount?.details_submitted;
       res.locals.country = req.stripeAccount.country;
