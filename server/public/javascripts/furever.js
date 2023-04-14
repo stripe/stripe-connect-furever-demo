93cd5588bb581a8663295e2179531cce10668dd9
Matt Roll <mroll@stripe.com>
2023-04-14 16:51:15 -0400

    move form to profile page

--- server/public/javascripts/furever.js
+++ server/public/javascripts/furever.js
@@ -8,27 +8,27 @@ function responsiveNavbar() {
   }
 }
 
-const annotationCards = document.querySelectorAll('.annotation > .card')
-
-annotationCards.forEach((annotationCard) => {
-  const annotationToggle = annotationCard.querySelector('.card-title > i')
-  if (annotationToggle !== null) {
-    annotationToggle.addEventListener('click', function (e) {
-      e.preventDefault();
-      const icon = annotationCard.querySelector('.card-title > i');
-      const isExpanded = icon.classList.contains('fa-chevron-up');
-      if (isExpanded) {
-        icon.classList.remove('fa-chevron-up');
-        icon.classList.add('fa-chevron-down');
-      } else {
-        icon.classList.add('fa-chevron-up');
-        icon.classList.remove('fa-chevron-down');
-      }
-      annotationCard.querySelector('.description').classList.toggle('expanded');
-      annotationCard.querySelector('form').classList.toggle('expanded');
-    });
-  }
-});
+// Toggle showing details on annotation cards
+const annotationToggle = document.querySelector(
+  '.annotation.card > .card-title > i'
+);
+const annotation = document.querySelector('.annotation.card');
+if (annotationToggle !== null) {
+  annotationToggle.addEventListener('click', function (e) {
+    e.preventDefault();
+    const icon = annotation.querySelector('.card-title > i');
+    const isExpanded = icon.classList.contains('fa-chevron-up');
+    if (isExpanded) {
+      icon.classList.remove('fa-chevron-up');
+      icon.classList.add('fa-chevron-down');
+    } else {
+      icon.classList.add('fa-chevron-up');
+      icon.classList.remove('fa-chevron-down');
+    }
+    annotation.querySelector('.description').classList.toggle('expanded');
+    annotation.querySelector('form').classList.toggle('expanded');
+  });
+}
 
 const searchParams = new URL(window.location.href).searchParams;
 if (searchParams.get('dev') === 'true') {
