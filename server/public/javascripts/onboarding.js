529c56eb209beff80c6695333c0dafbae1138ccf
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2023-04-28 08:56:51 -0700

    Rename onboarding event

--- server/public/javascripts/onboarding.js
+++ server/public/javascripts/onboarding.js
@@ -1,9 +1,9 @@
-const onOnboardingComplete = () => {
-  console.log('Onboarding complete! We redirect the user to the next page...');
+const onOnboardingExited = () => {
+  console.log('Onboarding exited! We redirect the user to the next page...');
   window.location.href = '/stripe/onboarded';
 };
 
 const onboardingElement = document.querySelector(
   'stripe-connect-account-onboarding'
 );
-onboardingElement.addEventListener('onboardingcomplete', onOnboardingComplete);
+onboardingElement.addEventListener('onboardingexited', onOnboardingExited);
