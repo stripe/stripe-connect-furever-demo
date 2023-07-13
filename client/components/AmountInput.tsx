1d9137773913c4fae8f53dc8a823cd33004e128c
Ian Harris <ianharris@stripe.com>
2023-07-12 21:07:51 -0600

    Extract reusable form components for amount and currency selection

---
+++ client/components/AmountInput.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {TextInput} from './FormInputs';
+
+type Props = {
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
+};
+
+const AmountInput = ({value, onChange}: Props) => {
+  return (
+    <TextInput
+      label="Amount"
+      type="number"
+      name="amount"
+      placeholder="If blank, a random amount is used"
+      inputProps={{
+        min: '1',
+        max: '1000',
+        pattern: 'd*',
+      }}
+      size="small"
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default AmountInput;
