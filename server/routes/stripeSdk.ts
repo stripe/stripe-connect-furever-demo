a8602466a116c8fb338a0eb474cb2d86d0f0bf93
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2023-07-19 09:58:49 -0700

    Update stripeSdk.ts

--- server/routes/stripeSdk.ts
+++ server/routes/stripeSdk.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config({path: './.env'});
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  // @ts-ignore If you wish to remain on your account's default API version, you may pass null or another version instead of the latest version, and add a @ts-ignore comment here and anywhere the types differ between API versions.
-  apiVersion: '2022-08-01; embedded_connect_beta=v1;unified_accounts_beta=v1',
+  // @ts-ignore
+  apiVersion: '2022-08-01',
 });
