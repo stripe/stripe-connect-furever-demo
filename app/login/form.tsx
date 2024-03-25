210d65be93af9b0b38ec5741c0c86618ae498dba
Steve Kaliski <stevekaliski@stripe.com>
2024-03-25 17:49:48 -0400

    add form management

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -1,29 +1,45 @@
 'use client';
 
-import React, {useState} from 'react';
+import * as React from 'react';
 import {signIn} from 'next-auth/react';
 import {useRouter} from 'next/navigation';
+import {Loader2} from 'lucide-react';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
+import {Button} from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
 
-type FormValues = {
-  email: string;
-  password: string;
-};
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
 
-export default function Form() {
+export default function SignupForm() {
   const router = useRouter();
 
-  const [formValues, setFormValues] = useState<FormValues>({
-    email: '',
-    password: '',
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
   });
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await signIn('login', {
-        email: formValues.email,
-        password: formValues.password,
+        email: values.email,
+        password: values.password,
         redirect: true,
       });
     } catch (error: any) {
@@ -32,54 +48,59 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-6">
-      <div className="flex flex-col space-y-2">
-        <label className="font-bold" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="border border-gray-300 rounded-md p-2"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="jenny.rosen@example.com"
-          required
-          value={formValues.email}
-          onChange={(event) =>
-            setFormValues((prev) => ({
-              ...prev,
-              email: event.target.value,
-            }))
-          }
-        />
-      </div>
-      <div className="flex flex-col space-y-2">
-        <label className="font-bold" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="border border-gray-300 rounded-md p-2"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="••••••••"
-          required
-          value={formValues.password}
-          onChange={(event) =>
-            setFormValues((prev) => ({
-              ...prev,
-              password: event.target.value,
-            }))
-          }
-        />
-      </div>
-      <div>
-        <input
-          className="bg-primary text-white p-2 rounded-md w-full font-bold"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="flex flex-col space-y-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel className="font-bold">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border border-gray-300 rounded-md p-2 placeholder:text-gray-400"
+                    placeholder="jenny.rosen@example.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <FormField
+            control={form.control}
+            name="password"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel className="font-bold">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border border-gray-300 rounded-md p-2 placeholder:text-gray-400"
+                    placeholder="••••••••"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
           type="submit"
-          value="Continue"
-        />
-      </div>
-    </form>
+          className={'bg-primary text-white p-2 rounded-md w-full font-bold'}
+        >
+          {!form.formState.isSubmitting && <>Continue</>}
+          {form.formState.isSubmitting && (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Submitting...
+            </>
+          )}
+        </Button>
+      </form>
+    </Form>
   );
 }
