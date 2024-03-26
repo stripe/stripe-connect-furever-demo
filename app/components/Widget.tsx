aa5875d156139aa7869e1a83fe07d869b567189d
Steve Kaliski <stevekaliski@stripe.com>
2024-03-26 16:58:00 -0400

    basic home page

---
+++ app/components/Widget.tsx
@@ -0,0 +1,10 @@
+const Widget = () => {
+  return (
+    <div className="bg-white shadow-md p-4 rounded-xl space-y-4">
+      <h1>Header</h1>
+      <p>Subheader</p>
+    </div>
+  );
+};
+
+export default Widget;
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          'min-h-screen bg-secondary font-sans antialiased',
+          'min-h-screen bg-offset font-sans antialiased',
           fontSans.variable
         )}
       >
