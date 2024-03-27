297184049821a6e327fbcd3722e55d3d4193ad99
Steve Kaliski <stevekaliski@stripe.com>
2024-03-27 08:57:48 -0400

    return null if loading

--- app/finance/page.tsx
+++ app/finance/page.tsx
@@ -108,7 +108,7 @@ export default function Finance() {
 
   const [subpage, setSubpage] = useState('transactions');
 
-  if (useFinancialAccountError) {
+  if (useFinancialAccountError || loading) {
     return null;
   }
 
