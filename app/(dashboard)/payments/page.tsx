f256ba6f6543668ddd23dceb2e11d5f7c396c86b
Steve Kaliski <stevekaliski@stripe.com>
2024-03-29 10:20:12 -0400

    add wigets to payments page

--- app/(dashboard)/payments/page.tsx
+++ app/(dashboard)/payments/page.tsx
@@ -3,11 +3,21 @@
 import {ConnectPayments} from '@stripe/react-connect-js';
 import Container from '@/app/components/Container';
 import EmbeddedComponentContainer from '@/app/components/EmbeddedComponentContainer';
+import MonthToDateWidget from '@/app/components/MonthToDateWidget';
+import CustomersWidget from '@/app/components/CustomersWidget';
 
 export default function Payouts() {
   return (
     <>
       <h1 className="text-3xl font-bold">Payments</h1>
+      <div className="flex flex-row space-x-8">
+        <div className="flex-1">
+          <MonthToDateWidget />
+        </div>
+        <div className="flex-1">
+          <CustomersWidget />
+        </div>
+      </div>
       <Container>
         <h1 className="text-lg font-bold">Recent payments</h1>
         <EmbeddedComponentContainer>
