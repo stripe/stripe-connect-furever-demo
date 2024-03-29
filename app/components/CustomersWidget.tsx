3eef33d4d1dc4898f63f78221388ed5cf2cbe22e
Steve Kaliski <stevekaliski@stripe.com>
2024-03-29 15:51:39 -0400

    add sparkline

--- app/components/CustomersWidget.tsx
+++ app/components/CustomersWidget.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import React from 'react';
 import Container from './Container';
 import {Badge} from '@/components/ui/badge';
+import {SparkLineChart} from '@mui/x-charts/SparkLineChart';
 
-const CustomersWidget = () => {
+type CustomersWidgetProps = {
+  chartWidth: number;
+};
+
+const CustomersWidget = ({chartWidth}: CustomersWidgetProps) => {
   return (
     <Container>
       <div className="flex flex-row justify-between">
@@ -11,9 +18,17 @@ const CustomersWidget = () => {
           <div className="flex flex-row space-x-2">
             <div className="font-bold text-xl">424</div>
             <Badge className="bg-success text-success-foreground rounded-md pb-0 pt-0 pl-1 pr-1">
-              +3.9%
+              +12.5%
             </Badge>
           </div>
+        </div>
+        <div>
+          <SparkLineChart
+            data={[3, 2, 10, 24, 8, 10, 14, 25, 30]}
+            height={50}
+            width={chartWidth}
+            colors={['black']}
+          />
         </div>
       </div>
     </Container>
