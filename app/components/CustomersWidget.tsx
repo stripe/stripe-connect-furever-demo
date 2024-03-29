2dd0278a3f0ec37281e088f5cc263ccc4fb69aab
Steve Kaliski <stevekaliski@stripe.com>
2024-03-29 10:36:25 -0400

    more badges, styles, and stuff

--- app/components/CustomersWidget.tsx
+++ app/components/CustomersWidget.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Container from './Container';
+import {Badge} from '@/components/ui/badge';
 
-const BalanceWidget = () => {
+const CustomersWidget = () => {
   return (
     <Container>
-      <div className="flex flex-row justify-between space-y-1">
-        <div>
+      <div className="flex flex-row justify-between">
+        <div className="space-y-1">
           <h1 className="font-bold text-subdued">Customers</h1>
-          <div className="font-bold text-xl">424</div>
+          <div className="flex flex-row space-x-2">
+            <div className="font-bold text-xl">424</div>
+            <Badge className="bg-success text-success-foreground rounded-md pb-0 pt-0 pl-1 pr-1">
+              +3.9%
+            </Badge>
+          </div>
         </div>
       </div>
     </Container>
   );
 };
 
-export default BalanceWidget;
+export default CustomersWidget;
