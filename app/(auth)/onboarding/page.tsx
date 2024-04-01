61ee3cbe3b8439c96ceafd9b2980a999da6cf332
Steve Kaliski <stevekaliski@stripe.com>
2024-04-01 13:09:08 -0400

    fix auth

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,21 +1,15 @@
 'use client';
 
-import {useRouter} from 'next/navigation';
-import {useSession} from 'next-auth/react';
 import {ConnectAccountOnboarding} from '@stripe/react-connect-js';
 import EmbeddedComponentContainer from '@/app/components/EmbeddedComponentContainer';
 import React from 'react';
 
 export default function Onboarding() {
-  const {update} = useSession();
-  const router = useRouter();
-
   return (
     <EmbeddedComponentContainer>
       <ConnectAccountOnboarding
-        onExit={async () => {
-          await update();
-          router.push('/');
+        onExit={() => {
+          window.location.href = '/';
         }}
       />
     </EmbeddedComponentContainer>
