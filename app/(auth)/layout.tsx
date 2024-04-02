80a4eb9d66f70dc8526015aad51a926ff4b2f371
Eric Liang <eliang@stripe.com>
2024-04-02 12:52:34 -0400

    polish, input focus

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import {usePathname} from 'next/navigation';
 import Container from '@/app/components/Container';
 import BackgroundImage from '@/public/background.jpg';
-import {ChevronRight} from 'lucide-react';
+import {ArrowRight} from 'lucide-react';
 
 export default function AuthLayout({
   children,
@@ -22,7 +22,7 @@ export default function AuthLayout({
   }
 
   return (
-    <>
+    <div className="relative">
       <div className="flex min-h-screen min-w-[926px] justify-center space-x-20 px-6 py-[120px]">
         <div className="flex w-[850px]">
           <div className="fixed max-w-sm space-y-4">
@@ -34,13 +34,13 @@ export default function AuthLayout({
               height={23}
             />
             <h1 className="text-4xl font-bold">{header}</h1>
-            <p className="text-xl">{subheader}</p>
+            <p className="text-xl text-subdued">{subheader}</p>
             <Link
               href="mailto:support@example.com"
-              className="flex flex-row items-center"
+              className="flex flex-row items-center gap-x-1"
             >
               <div className="font-bold text-secondary">Contact support</div>
-              <ChevronRight color="#f26552" size={18} className="mt-[1px]" />
+              <ArrowRight color="#f26552" size={18} className="mt-[1px]" />
             </Link>
           </div>
           <div className="ml-auto min-w-96">
@@ -59,6 +59,6 @@ export default function AuthLayout({
         sizes="100vw"
         className="fixed z-[-1] h-full w-full min-w-[926px] overflow-hidden object-cover"
       />
-    </>
+    </div>
   );
 }
--- app/(dashboard)/finances/page.tsx
+++ app/(dashboard)/finances/page.tsx
@@ -45,7 +45,7 @@ export default function Finances() {
   return (
     <>
       <Container>
-        <h1 className="mb-6 text-lg font-bold">Financial account</h1>
+        <h1 className="mb-2 text-lg font-bold">Financial account</h1>
         <EmbeddedComponentContainer>
           {displayFinancialAccount && (
             <ConnectFinancialAccount financialAccount={financialAccount} />
