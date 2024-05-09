8b004bdda508392728d13ff0cb015175d68adb33
Eric Liang <eliang@stripe.com>
2024-05-09 14:43:42 -0400

    remove comment

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -93,7 +93,6 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      // className={cn(error && 'text-destructive', className)}
       htmlFor={formItemId}
       {...props}
     />
