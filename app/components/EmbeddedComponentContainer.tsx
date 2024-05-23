086831e0022aa78b3817ceeaded573149709548b
Eric Liang <eliang@stripe.com>
2024-05-23 11:46:34 -0400

    styled component border hover interaction

--- app/components/EmbeddedComponentContainer.tsx
+++ app/components/EmbeddedComponentContainer.tsx
@@ -1,4 +1,5 @@
 import {useEmbeddedComponentBorder} from '@/app/hooks/EmbeddedComponentBorderProvider';
+import { ChevronRight } from 'lucide-react';
 
 const EmbeddedComponentContainer = ({
   children, className
@@ -10,8 +11,18 @@ const EmbeddedComponentContainer = ({
 
   return (
     <div
-      className={`${enableBorder ? 'rounded-lg border-2 border-dashed border-[#7F81FA] p-[4px]' : 'p-[6px]'} transition-border duration-200 ${className}`}
+      className={`${enableBorder ? 'rounded-lg border-2 border-dashed border-[#7F81FA] p-[4px]' : 'p-[6px]'} transition-border duration-200 relative group ${className}`}
     >
+      <div className="absolute top-1 z-50 flex gap-2 opacity-0 group-hover:opacity-100 transition duration-150">
+        <div className="bg-[#7F81FA] shadow-lg text-white font-mono font-bold rounded py-0.5 px-1.5">Component name</div>
+        <a
+          className="bg-white shadow-lg rounded font-medium py-0.5 px-1.5 flex gap-1 items-center hover:opacity-70"
+          href=""
+          target="_blank">
+          View in docs
+          <ChevronRight size="16" />
+        </a>
+      </div>
       {children}
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,6 +77,7 @@ const config = {
     },
     fontFamily: {
       sans: ['Sohne', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      mono: ['ui-monospace', 'SFMono-Regular'],
     },
   },
   plugins: [require('tailwindcss-animate')],
