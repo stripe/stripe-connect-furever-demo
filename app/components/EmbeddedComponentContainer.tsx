1c904040d6ee00385f2e4ab83cd09d3b271c322c
Eric Liang <eliang@stripe.com>
2024-05-23 11:50:52 -0400

    dont show details if border not enabled

--- app/components/EmbeddedComponentContainer.tsx
+++ app/components/EmbeddedComponentContainer.tsx
@@ -9,10 +9,10 @@ const EmbeddedComponentContainer = ({
 }) => {
   const {enableBorder} = useEmbeddedComponentBorder();
 
-  return (
-    <div
-      className={`${enableBorder ? 'rounded-lg border-2 border-dashed border-[#7F81FA] p-[4px]' : 'p-[6px]'} transition-border duration-200 relative group ${className}`}
-    >
+  const ComponentDetails = () => {
+    if (!enableBorder) { return; }
+
+    return (
       <div className="absolute top-1 z-50 flex gap-2 opacity-0 group-hover:opacity-100 transition duration-150">
         <div className="bg-[#7F81FA] shadow-lg text-white font-mono font-bold rounded py-0.5 px-1.5">Component name</div>
         <a
@@ -23,6 +23,14 @@ const EmbeddedComponentContainer = ({
           <ChevronRight size="16" />
         </a>
       </div>
+    );
+  };
+
+  return (
+    <div
+      className={`${enableBorder ? 'rounded-lg border-2 border-dashed border-[#7F81FA] p-[4px]' : 'p-[6px]'} transition-border duration-200 relative group ${className}`}
+    >
+      <ComponentDetails />
       {children}
     </div>
   );
