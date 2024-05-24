f857c5d3b5a1762b24d1ecafa3d76da75011e709
jojo-stripe <124314569+jojo-stripe@users.noreply.github.com>
2024-05-24 10:15:42 -0700

    Add bank account collection form for custom accounts (#149)
    * call stripe logout
    
    * baf

--- app/(dashboard)/settings/layout.tsx
+++ app/(dashboard)/settings/layout.tsx
@@ -4,12 +4,14 @@ import {signOut} from 'next-auth/react';
 import SubNav from '@/app/components/SubNav';
 import {Button} from '@/components/ui/button';
 import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar';
+import {useConnectJSContext} from '@/app/hooks/EmbeddedComponentProvider';
 
 export default function SettingsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const connectJSContext = useConnectJSContext();
   return (
     <>
       <header className="flex flex-col justify-between md:flex-row">
@@ -28,7 +30,10 @@ export default function SettingsLayout({
             <Button
               className="text-md ml-2 self-end p-2 hover:bg-white/80 hover:text-primary"
               variant="ghost"
-              onClick={() => signOut({callbackUrl: '/'})}
+              onClick={async () => {
+                await connectJSContext.connectInstance?.logout();
+                signOut({callbackUrl: '/'});
+              }}
             >
               Sign out
             </Button>
