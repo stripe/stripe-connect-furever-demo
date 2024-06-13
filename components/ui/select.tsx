18f2bccd816b1be644c49ce87433cb2e2b69cce1
jojo-stripe <124314569+jojo-stripe@users.noreply.github.com>
2024-06-13 09:16:59 -0700

    UI tweaks for front page (#157)
    * front page tweaks
    
    * bold

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -124,9 +124,7 @@ const SelectItem = React.forwardRef<
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <SelectPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
-      </SelectPrimitive.ItemIndicator>
+      <SelectPrimitive.ItemIndicator></SelectPrimitive.ItemIndicator>
     </span>
 
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
