72ac54e1cb3b84f7fb694acbfd4f9a4a376c03ec
riquelme-stripe <riquelme@stripe.com>
2024-07-16 09:12:44 -0700

    Add Notification Banner Callback (#179)
    * Added Changes to callback
    
    * updated callback render
    
    * Updated styling
    
    * force hide notif banner background
    
    * removed unused comments
    
    * Addressed Yarn Lock
    
    ---------
    
    Signed-off-by: riquelme-stripe <riquelme@stripe.com>
    Co-authored-by: Eric Liang <eliang@stripe.com>

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+import React from 'react';
+import {useState} from 'react';
 import Schedule from '@/app/components/Schedule';
 import BalanceWidget from '@/app/components/BalanceWidget';
 import RecentPaymentsWidget from '@/app/components/RecentPaymentsWidget';
@@ -20,17 +22,35 @@ export default function Dashboard() {
 
   const BREAKPOINT = 1190;
 
+  const [showBanner, setShowBanner] = React.useState(false);
+
+  const renderConditionallyCallback = (response: {
+    total: number;
+    actionRequired: number;
+  }) => {
+    if (response && response.total > 0) {
+      setShowBanner(true);
+    } else {
+      setShowBanner(false);
+    }
+  };
+
   return (
     <>
       <h1 className="text-3xl font-bold">Woof woof, {name || 'human'}!</h1>
+      <div className={`${showBanner ? 'flex' : 'hidden'} flex-col`}>
+        <EmbeddedComponentContainer
+          componentName="NotificationBanner"
+          className="overflow-hidden rounded-lg px-0 py-0 pb-1"
+        >
+          <ConnectNotificationBanner
+            onNotificationsChange={renderConditionallyCallback}
+          />
+        </EmbeddedComponentContainer>
+      </div>
+
       <div className="flex flex-col items-start gap-2 md:gap-5 xl:flex-row">
         <Container className="flex w-full flex-1 flex-col p-5">
-          <EmbeddedComponentContainer
-            componentName="NotificationBanner"
-            className="-m-2 mb-0.5"
-          >
-            <ConnectNotificationBanner />
-          </EmbeddedComponentContainer>
           <Schedule />
         </Container>
         <div className="-order-1 flex w-full flex-col gap-2 md:gap-4 xl:order-2 xl:w-[30%]">
