9eddfc91b79938b51d6597bdeb5d90593ed043b1
Luca Loguercio <lucaloguercio@stripe.com>
2024-08-29 17:34:24 -0400

    Removing the API version beta flag

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   // @ts-ignore
-  apiVersion: '2023-10-16; embedded_connect_beta=v2',
+  apiVersion: '2023-10-16',
 });
