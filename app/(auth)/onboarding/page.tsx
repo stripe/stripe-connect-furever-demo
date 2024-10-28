136bf123a26f197cce2bf3e8abfd54be0584a5c3
Calvin Dong <calvind@stripe.com>
2024-10-28 17:13:45 -0400

    remove unused

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -3,12 +3,8 @@
 import {ConnectAccountOnboarding} from '@stripe/react-connect-js';
 import EmbeddedComponentContainer from '@/app/components/EmbeddedComponentContainer';
 import React from 'react';
-import {useSession} from 'next-auth/react';
 
 export default function Onboarding() {
-  const {data: session} = useSession();
-  const stripeAccount = session?.user.stripeAccount;
-
   return (
     <EmbeddedComponentContainer componentName="AccountOnboarding">
       <ConnectAccountOnboarding
