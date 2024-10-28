e49848188f882254e48da456525f885568e11493
calvind-stripe <102095083+calvind-stripe@users.noreply.github.com>
2024-10-28 17:38:23 -0400

    Remove custom bank account flow (#192)
    * Fix custom
    
    * remove unused

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -3,26 +3,13 @@
 import {ConnectAccountOnboarding} from '@stripe/react-connect-js';
 import EmbeddedComponentContainer from '@/app/components/EmbeddedComponentContainer';
 import React from 'react';
-import {useSession} from 'next-auth/react';
 
 export default function Onboarding() {
-  const {data: session} = useSession();
-  const stripeAccount = session?.user.stripeAccount;
-
-  const isCustom =
-    stripeAccount?.controller?.stripe_dashboard?.type === 'none' &&
-    stripeAccount?.controller?.losses?.payments === 'application' &&
-    stripeAccount?.controller?.requirement_collection === 'application';
-
   return (
     <EmbeddedComponentContainer componentName="AccountOnboarding">
       <ConnectAccountOnboarding
         onExit={() => {
-          if (isCustom) {
-            window.location.href = '/bankaccountform';
-          } else {
-            window.location.href = '/home?shownux=true';
-          }
+          window.location.href = '/home?shownux=true';
         }}
       />
     </EmbeddedComponentContainer>
