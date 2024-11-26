b4361c2c1d8d7f361f42d7def56ea46d851f0a01
Kushal Shah <kushalshah@stripe.com>
2024-11-26 13:45:48 -0800

    fix python

--- scripts/setup-accounts.py
+++ scripts/setup-accounts.py
@@ -1709,7 +1709,7 @@ def generate_account_sessions(account):
     # Generate 5 account sessions to hide the support ticket API log
     for _ in range(5):
         try:
-            stripe.v2.core.accountsession.create(
+            stripe.AccountSession.create(
                 account=account.id, components={"account_onboarding": {"enabled": True}}
             )
         except stripe.error.StripeError as e:
