e2a02ae088a4032d269c21099875fa9214db7e8d
eliang-stripe <99840257+eliang-stripe@users.noreply.github.com>
2025-05-05 11:17:06 -0400

    Add balance component to homepage (#227)
    * added balance component to homepage
    
    * update breakpoints
    
    * add tax component links
    
    * remove unusued component
    
    * remove unused widgets

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React from 'react';
-import {useState} from 'react';
 import Schedule from '@/app/components/Schedule';
-import BalanceWidget from '@/app/components/BalanceWidget';
-import RecentPaymentsWidget from '@/app/components/RecentPaymentsWidget';
 import MonthToDateWidget from '@/app/components/MonthToDateWidget';
 import CustomersWidget from '@/app/components/CustomersWidget';
 import EmbeddedComponentContainer from '@/app/components/EmbeddedComponentContainer';
-import {ConnectNotificationBanner} from '@stripe/react-connect-js';
+import {
+  ConnectNotificationBanner,
+  ConnectBalances,
+} from '@stripe/react-connect-js';
 import {useSession} from 'next-auth/react';
 import {redirect} from 'next/navigation';
 import Container from '@/app/components/Container';
@@ -55,14 +55,17 @@ export default function Dashboard() {
         <Container className="flex w-full flex-1 flex-col p-5">
           <Schedule />
         </Container>
-        <div className="-order-1 flex w-full flex-col gap-2 md:gap-4 xl:order-2 xl:w-[30%]">
+        <div className="-order-1 flex w-full min-w-[370px] flex-col gap-2 md:gap-4 xl:order-2 xl:w-[30%]">
           <div className="flex flex-grow flex-col gap-2 md:gap-4 md:max-xl:flex-row">
-            <BalanceWidget />
+            <Container className="flex-grow px-3 py-4">
+              <EmbeddedComponentContainer componentName="Balances">
+                <ConnectBalances />
+              </EmbeddedComponentContainer>
+            </Container>
             <CapitalFinancingPromotionSection
               layout="banner"
               className="w-full px-5"
             />
-            <RecentPaymentsWidget />
           </div>
           <h2 className="hidden pt-4 text-lg font-bold xl:block">
             Performance
