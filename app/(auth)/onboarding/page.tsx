30b903e5447256e48017684a1330616188539af3
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2025-05-21 13:03:04 -0700

    Initializing embedded

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,18 +1,51 @@
 'use client';
 
-import {useAccountLinkCreate} from '@/app/hooks/useAccountLinkCreate';
 import React from 'react';
+import {loadConnectAndInitialize} from '@stripe/connect-js';
+import {
+  ConnectAccountOnboarding,
+  ConnectComponentsProvider,
+} from '@stripe/react-connect-js';
 
 export default function Onboarding() {
-  const {isLoading, error, data} = useAccountLinkCreate();
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+  const [stripeConnectInstance] = React.useState(() => {
+    const fetchClientSecret = async () => {
+      const response = await fetch('/api/create_account_session', {
+        method: 'POST',
+      });
+      if (!response.ok) {
+        console.error('An error occurred: ');
+        throw new Error('Failed to fetch account session');
+      } else {
+        const {client_secret} = await response.json();
+        return client_secret;
+      }
+    };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-  if (isLoading || !data) {
-    return <div>Loading...</div>;
-  }
+    return loadConnectAndInitialize({
+      publishableKey:
+        'pk_test_51MZRIlLirQdaQn8EJpw9mcVeXokTGaiV1ylz5AVQtcA0zAkoM9fLFN81yQeHYBLkCiID1Bj0sL1Ngzsq9ksRmbBN00O3VsIUdQ',
+      fetchClientSecret: fetchClientSecret,
+    });
+  });
 
-  const {url} = data;
-  return <a href={url}>Open account link</a>;
+  return (
+    <>
+      {errorMessage ? (
+        <div>{`Error: ${errorMessage}`}</div>
+      ) : (
+        <div className="container">
+          <ConnectComponentsProvider connectInstance={stripeConnectInstance}>
+            <ConnectAccountOnboarding
+              onExit={() => {
+                console.log('Onboarding flow exited. Redirecting to home...');
+                window.location.href = '/home';
+              }}
+            />
+          </ConnectComponentsProvider>
+        </div>
+      )}
+    </>
+  );
 }
---
+++ app/api/create_account_session/route.ts
@@ -0,0 +1,31 @@
+import {getServerSession} from 'next-auth/next';
+import {authOptions} from '@/lib/auth';
+import {stripe} from '@/lib/stripe';
+
+export async function POST() {
+  const session = await getServerSession(authOptions);
+  let stripeAccountId = session?.user?.stripeAccount?.id;
+
+  if (!stripeAccountId) {
+    return new Response(
+      JSON.stringify({
+        error: 'No Stripe account found for this user',
+      }),
+      {status: 400}
+    );
+  }
+
+  const accountLink = await stripe.accountSessions.create({
+    account: stripeAccountId,
+    components: {
+      account_onboarding: {
+        enabled: true,
+      },
+    },
+  });
+
+  return new Response(JSON.stringify(accountLink), {
+    status: 200,
+    headers: {'Content-Type': 'application/json'},
+  });
+}
