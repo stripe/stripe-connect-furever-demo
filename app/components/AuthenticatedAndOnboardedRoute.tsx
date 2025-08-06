0cab575387ceb14eecd5db34f5fc3a599e1cadc6
kaiying-stripe <110323300+kaiying-stripe@users.noreply.github.com>
2025-08-06 13:57:17 -0700

    prevent rendering the dashhoard when not onboarded as it'll redirect away (#248)
    * prevent rendering the furever dash when not onboarded as it'll redirect away
    
    * address feedback

--- app/components/AuthenticatedAndOnboardedRoute.tsx
+++ app/components/AuthenticatedAndOnboardedRoute.tsx
@@ -3,22 +3,40 @@
 import {useSession} from 'next-auth/react';
 import {useRouter} from 'next/navigation';
 import {useEffect} from 'react';
+import {LoaderCircle} from 'lucide-react';
+
+const LoadingView = () => {
+  return (
+    <div className="h-64 content-center">
+      <LoaderCircle className="mx-auto animate-spin" size={30} />
+    </div>
+  );
+};
+
+const RedirectToOnboarding = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/onboarding');
+  }, [router]);
+
+  return <LoadingView />;
+};
 
 export default function AuthenticatedAndOnboardedRoute({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter();
   const {data: session, status} = useSession();
 
-  useEffect(() => {
-    if (session?.user?.stripeAccount?.details_submitted === false) {
-      router.push('/onboarding');
-    }
-  }, [session, router]);
-  if (!session || !session.user) {
-    return null;
+  const isLoading = !session || !session.user;
+  const shouldRedirect = !session?.user?.stripeAccount?.details_submitted;
+
+  if (isLoading) {
+    return <LoadingView />;
+  } else if (shouldRedirect) {
+    return <RedirectToOnboarding />;
   }
 
   return <>{children}</>;
