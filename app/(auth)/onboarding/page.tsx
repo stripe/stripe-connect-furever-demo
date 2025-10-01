8670d7f13b2f025ddc8798fc38e0622476436275
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2025-10-01 13:48:56 -0700

    Avoid flicker by using a specific redirect message

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -5,10 +5,20 @@ import EmbeddedComponentContainer from '@/app/components/EmbeddedComponentContai
 import React from 'react';
 
 export default function Onboarding() {
+  const [hasCompletedOnboarding, setHasCompletedOnboarding] =
+    React.useState(false);
+
+  if (hasCompletedOnboarding) {
+    return <div>Onboarding finished! Redirecting to dashboard...</div>;
+  }
+
   return (
     <EmbeddedComponentContainer componentName="AccountOnboarding">
       <ConnectAccountOnboarding
         onExit={() => {
+          // Since redirecting takes some time, we set the state first to hide the onboarding component to avoid showing the default "success" screen
+          setHasCompletedOnboarding(true);
+
           window.location.href = '/home?shownux=true';
         }}
       />
