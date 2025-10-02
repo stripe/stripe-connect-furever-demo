980d643118aaba03fabdde3defe0c7f7255af944
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2025-10-02 13:46:07 -0700

    Avoid flicker by using a specific redirect message instead of the onboarding component (#252)
    * Avoid flicker by using a specific redirect message
    
    * Use a spinner

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -3,12 +3,28 @@
 import {ConnectAccountOnboarding} from '@stripe/react-connect-js';
 import EmbeddedComponentContainer from '@/app/components/EmbeddedComponentContainer';
 import React from 'react';
+import {LoaderCircle} from 'lucide-react';
 
 export default function Onboarding() {
+  const [hasCompletedOnboarding, setHasCompletedOnboarding] =
+    React.useState(false);
+
+  if (hasCompletedOnboarding) {
+    // If we render this, we are in the process of redirecting
+    return (
+      <div className="flex w-full items-center justify-center">
+        <LoaderCircle className="mr-1 animate-spin" size={20} />
+      </div>
+    );
+  }
+
   return (
     <EmbeddedComponentContainer componentName="AccountOnboarding">
       <ConnectAccountOnboarding
         onExit={() => {
+          // Since redirecting takes some time, we set the state first to hide the onboarding component to avoid showing the default "success" screen
+          setHasCompletedOnboarding(true);
+
           window.location.href = '/home?shownux=true';
         }}
       />
