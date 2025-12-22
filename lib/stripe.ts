bf627196efec3d39318d2e4a051b936238336eee
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2025-12-22 09:11:09 -0800

    Update to private preview package (#258)

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-export const latestApiVersion: Stripe.LatestApiVersion = '2025-04-30.preview';
+export const latestApiVersion: Stripe.LatestApiVersion = '2025-12-15.preview';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: latestApiVersion,
