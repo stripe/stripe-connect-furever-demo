563a6029cc5df0d48ab7736ef0b8472d0e864531
Jeet Patel <jeet@stripe.com>
2026-04-16 12:16:03 -0400

    Update Capital Financing ToolsPanel to support Line of Credit

--- app/components/testdata/Financing/ManageFinancing.tsx
+++ app/components/testdata/Financing/ManageFinancing.tsx
@@ -1,8 +1,30 @@
+import {Form} from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import React from 'react';
-import {OfferState} from './types';
-import {CreateFlexLoanButton} from './CreateFlexLoanButton';
-import {LoaderCircle} from 'lucide-react';
+import {useForm} from 'react-hook-form';
+import {ManageFinancingOfferFormField} from './ManageFinancingOfferFormField';
+import {
+  enumValueToSentenceCase,
+  FinancingOfferProductType,
+  TestmodeFinancingForm,
+  OfferState,
+  TestmodeFinancingFormType,
+} from './types';
+import {
+  STATES_FOR_PRODUCT_TYPE,
+  FINANCING_OFFER_PRODUCT_TYPES_ARRAY,
+  PRODUCT_TYPE_CREATE_URLS,
+  LINE_OF_CREDIT_PRODUCT_TYPES_ARRAY,
+} from './constants';
 import {TransitionFinancingButton} from './TransitionFinancingButton';
+import {Link} from '@/components/ui/link';
+import {ExternalLinkIcon} from '@radix-ui/react-icons';
 
 export default function ManageFinancing({classes}: {classes?: string}) {
   const [loading, setLoading] = React.useState(true);
@@ -10,80 +32,259 @@ export default function ManageFinancing({classes}: {classes?: string}) {
     undefined
   );
 
-  const response = React.useCallback(() => {
+  const [hasLineOfCreditFeature, setHasLineOfCreditFeature] =
+    React.useState(false);
+  const [hasLineOfCreditLine, setHasLineOfCreditLine] = React.useState(false);
+
+  const form = useForm<TestmodeFinancingForm>({
+    defaultValues: {
+      financingOfferType: 'flex_loan',
+      offerState: 'delivered',
+    },
+  });
+
+  React.useMemo(() => {
     if (offerState === undefined) {
-      fetch('/api/capital/get_financing_offer', {
-        method: 'get',
-      })
-        .then(async (res) => {
-          const status = (await res.json()).offer?.status;
-          setOfferState(status || 'no_offer');
-        })
-        .catch(async (err) => {
-          // Handle errors on the client side here
-          console.warn('An error occurred: ', err);
+      Promise.allSettled([
+        fetch('/api/capital/get_financing_offer', {
+          method: 'get',
+        }),
+        fetch('/api/capital/get_line_of_credit_summary', {
+          method: 'get',
+        }),
+      ])
+        .then(async ([offer, summary]) => {
+          if (summary.status === 'fulfilled') {
+            if (summary.value.ok) {
+              const summaryData = await summary.value.json();
+              setHasLineOfCreditFeature(true);
+              setHasLineOfCreditLine(summaryData.line_of_credit !== null);
+            } else {
+              const message = await summary.value.text();
+              if (
+                message.includes(
+                  'You do not have permission to pass this beta header'
+                )
+              ) {
+                setHasLineOfCreditFeature(false);
+                setHasLineOfCreditLine(false);
+              } else {
+                console.warn('An error occurred: ', message);
+              }
+            }
+          } else {
+            console.warn('An error occurred: ', summary.reason.message);
+          }
+
+          if (offer.status === 'fulfilled') {
+            if (offer.value.ok) {
+              const status = (await offer.value.json()).offer?.status;
+              setOfferState(status || 'no_offer');
+            } else {
+              const message = await offer.value.text();
+              console.warn('An error occurred: ', message);
+            }
+          } else {
+            console.warn('An error occurred: ', offer.reason.message);
+          }
         })
         .finally(() => {
           setLoading(false);
         });
     }
   }, [offerState]);
-  response();
 
-  if (!offerState && !loading) {
+  if (!offerState || hasLineOfCreditLine) {
     return undefined;
   }
 
+  const formFinancingOfferType = form.watch('financingOfferType');
+  const formOfferState = form.watch('offerState');
+
+  const statesForProductType = STATES_FOR_PRODUCT_TYPE[formFinancingOfferType];
+
+  // If the product type changes and the offer state is not valid for the new product type, set the offer state to the first valid state for the new product type
+  if (!statesForProductType.includes(formOfferState)) {
+    form.setValue('offerState', statesForProductType[0]);
+  }
+
+  const offerStateToFormType: {
+    [key in OfferState]: TestmodeFinancingFormType | undefined;
+  } = {
+    no_offer: 'create',
+    fully_repaid: 'create',
+    rejected: 'create',
+    canceled: 'create',
+    completed: 'create',
+    expired: 'create',
+    revoked: 'create',
+
+    accepted: 'approve_reject',
+    paid_out: 'fully_repay',
+
+    undelivered: 'expire',
+    delivered: 'expire',
+  };
+
+  const formType = offerStateToFormType[offerState || 'no_offer'];
+  const createUrl = PRODUCT_TYPE_CREATE_URLS[formFinancingOfferType];
+
+  const supportedProductTypes = FINANCING_OFFER_PRODUCT_TYPES_ARRAY;
+
   return (
-    <>
-      {!loading && offerState && (
+    <Form {...form}>
+      {formType === 'create' && (
         <>
-          <CreateFlexLoanButton
-            classes={classes}
-            offerState={offerState}
-            visibleForOfferStates={[
-              'canceled',
-              'completed',
-              'expired',
-              'fully_repaid',
-              'rejected',
-              'revoked',
-              'no_offer',
-            ]}
+          <ManageFinancingOfferFormField
+            name="financingOfferType"
+            label={
+              <div>
+                <Link
+                  href="https://docs.stripe.com/capital/how-capital-for-platforms-works#types-of-financing-offers"
+                  target="_blank"
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '2px',
+                  }}
+                >
+                  {'Product Type'}
+                  <ExternalLinkIcon className="h-3 w-3" />
+                </Link>
+              </div>
+            }
+            form={form}
+            loading={loading}
+            render={({field}) => {
+              return (
+                <Select
+                  {...field}
+                  onValueChange={(val) =>
+                    form.setValue(
+                      'financingOfferType',
+                      val as FinancingOfferProductType
+                    )
+                  }
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger
+                    className="w-[120px] text-xs"
+                    disabled={loading}
+                  >
+                    <SelectValue className="text-xs">
+                      {enumValueToSentenceCase(field.value)}
+                    </SelectValue>
+                  </SelectTrigger>
+
+                  <SelectContent className="z-[100] text-xs">
+                    {supportedProductTypes.map((productType) => (
+                      <SelectItem
+                        value={productType}
+                        key={productType}
+                        className="z-[130] text-xs"
+                        disabled={
+                          !hasLineOfCreditFeature &&
+                          LINE_OF_CREDIT_PRODUCT_TYPES_ARRAY.includes(
+                            productType as any
+                          )
+                        }
+                      >
+                        {enumValueToSentenceCase(productType)}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              );
+            }}
+          />
+          <ManageFinancingOfferFormField
+            name="offerState"
+            label="Offer State"
+            form={form}
+            loading={loading}
+            render={({field}) => {
+              return (
+                <Select
+                  {...field}
+                  onValueChange={(val) =>
+                    form.setValue('offerState', val as OfferState)
+                  }
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger
+                    className="w-[120px] text-xs"
+                    disabled={loading}
+                  >
+                    <SelectValue className="text-xs">
+                      {enumValueToSentenceCase(field.value)}
+                    </SelectValue>
+                  </SelectTrigger>
+
+                  <SelectContent className="z-[100] text-xs">
+                    {STATES_FOR_PRODUCT_TYPE[
+                      form.watch('financingOfferType')
+                    ].map((productType) => (
+                      <SelectItem
+                        value={productType}
+                        key={productType}
+                        className="z-[100] text-xs"
+                      >
+                        {enumValueToSentenceCase(productType)}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              );
+            }}
           />
 
           <TransitionFinancingButton
-            label={'Expire financing offer'}
-            fetchUrl="/api/capital/expire_test_financing"
-            visibleForOfferStates={['delivered']}
-            offerState={offerState}
-            classes={classes}
+            label={'Create'}
+            fetchUrl={createUrl}
+            fetchBody={{
+              offerState: form.watch('offerState'),
+            }}
+            form={form}
           />
+        </>
+      )}
+      {formType === 'expire' && (
+        <>
+          <TransitionFinancingButton
+            label={'Expire offer'}
+            fetchUrl="/api/capital/expire_test_financing"
+            form={form}
+          />
+        </>
+      )}
 
+      {formType === 'approve_reject' && (
+        <>
           <TransitionFinancingButton
             label={'Approve financing application'}
             fetchUrl="/api/capital/approve_test_financing"
-            visibleForOfferStates={['accepted']}
-            offerState={offerState}
             classes={classes}
           />
           <TransitionFinancingButton
             label={'Reject financing application'}
             fetchUrl="/api/capital/reject_test_financing"
-            visibleForOfferStates={['accepted']}
-            offerState={offerState}
             classes={classes}
-          />
-
-          <TransitionFinancingButton
-            label={'Fully repay financing'}
-            fetchUrl="/api/capital/fully_repay_test_financing"
-            visibleForOfferStates={['paid_out']}
-            offerState={offerState}
-            classes={classes}
+            form={form}
           />
         </>
       )}
-    </>
+
+      {formType === 'fully_repay' && (
+        <>
+          <TransitionFinancingButton
+            label={'Fully repay financing'}
+            fetchUrl="/api/capital/fully_repay_test_financing"
+            classes={classes}
+            form={form}
+          />
+        </>
+      )}
+    </Form>
   );
 }
--- app/components/testdata/Financing/TransitionFinancingButton.tsx
+++ app/components/testdata/Financing/TransitionFinancingButton.tsx
@@ -1,34 +1,30 @@
-import {Button} from '@/components/ui/button';
-import {LoaderCircle} from 'lucide-react';
-import React from 'react';
-import Stripe from 'stripe';
-import {OfferState} from './types';
-import {UseFormReturn} from 'react-hook-form';
+import { Button } from '@/components/ui/button'
+import { LoaderCircle } from 'lucide-react'
+import React from 'react'
+import Stripe from 'stripe'
+import { OfferState } from './types'
+import { UseFormReturn } from 'react-hook-form'
 
-const OFFER_STATES_TO_DISPLAY_ON: OfferState[] = ['accepted'];
+const OFFER_STATES_TO_DISPLAY_ON: OfferState[] = ['accepted']
 
 export function TransitionFinancingButton({
   classes,
-  offerState,
   label,
   fetchUrl,
-  visibleForOfferStates,
   fetchMethod: method = 'POST',
   fetchBody = {},
   form,
 }: {
-  offerState: OfferState;
-  label: string;
-  fetchUrl: string;
-  visibleForOfferStates: OfferState[];
-  classes?: string;
-  fetchMethod?: string;
-  fetchBody?: {};
-  form?: UseFormReturn<any>;
+  label: string
+  fetchUrl: string
+  classes?: string
+  fetchMethod?: string
+  fetchBody?: {}
+  form?: UseFormReturn<any>
 }) {
-  const [buttonLoading, setButtonLoading] = React.useState(false);
+  const [buttonLoading, setButtonLoading] = React.useState(false)
   const onClick = async () => {
-    setButtonLoading(true);
+    setButtonLoading(true)
     try {
       const res = await fetch(fetchUrl, {
         method,
@@ -36,34 +32,31 @@ export function TransitionFinancingButton({
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(fetchBody),
-      });
-      setButtonLoading(false);
+      })
+      setButtonLoading(false)
 
       if (res.ok) {
-        window.location.reload();
+        window.location.reload()
       }
     } catch (e) {
-      console.log(`Error attempting to \`${label}\`: `, e);
-      setButtonLoading(false);
+      console.log(`Error attempting to \`${label}\`: `, e)
+      setButtonLoading(false)
     }
-  };
-
-  if (visibleForOfferStates.includes(offerState)) {
-    return (
-      <Button
-        className={`${classes || 'border'}`}
-        variant="secondary"
-        onClick={form ? form.handleSubmit(onClick) : onClick}
-        disabled={buttonLoading}
-        size="sm"
-      >
-        {label}
-        {buttonLoading && (
-          <LoaderCircle className="ml-2 animate-spin items-center" size={20} />
-        )}
-      </Button>
-    );
-  } else {
-    return undefined;
   }
+
+  return (
+    <Button
+      className={`${classes || 'border'}`}
+      variant="secondary"
+      onClick={form ? form.handleSubmit(onClick) : onClick}
+      disabled={buttonLoading}
+      size="sm"
+    >
+      {label}
+      {buttonLoading && (
+        <LoaderCircle className="ml-2 animate-spin items-center" size={20} />
+      )}
+    </Button>
+  )
+
 }
