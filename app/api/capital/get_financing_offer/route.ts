8b67bab646aacbbbfb682b8aa9a4832bc7e3e583
Jeet Patel <jeet@stripe.com>
2026-04-24 14:13:22 -0400

    Support Line of Credit applications by using Line of Credit API preview header (with fallback)

--- app/api/capital/get_financing_offer/route.ts
+++ app/api/capital/get_financing_offer/route.ts
@@ -14,12 +14,46 @@ export async function GET() {
 
     const connected_account = session.user.stripeAccountId;
 
-    const offer = (
-      await stripe.capital.financingOffers.list({
-        connected_account: connected_account,
-        limit: 1,
-      })
-    ).data.at(0);
+    const overrideApiVersion =
+      '2026-03-25.dahlia; capital_line_of_credit_preview=v1';
+
+    const offer = await stripe.capital.financingOffers
+      .list(
+        {
+          connected_account: connected_account,
+          limit: 1,
+        },
+        {
+          apiVersion: overrideApiVersion,
+        }
+      )
+      .then(
+        (response) => {
+          return response.data.at(0);
+        },
+        // fallback to default API version if the override API version is not supported by the platform
+        async () => {
+          console.log(
+            'v1/capital/financing_offers: Unable to use line of credit preview API version. Falling back to default API version.'
+          );
+          return (
+            await stripe.capital.financingOffers.list({
+              connected_account: connected_account,
+              limit: 1,
+            })
+          ).data.at(0);
+        }
+      )
+      .catch((reason) => {
+        console.error(
+          'An error occurred when calling the Stripe API to list financing offers',
+          reason
+        );
+        return new Response(JSON.stringify(reason), {
+          status: 500,
+          headers: {'Content-Type': 'application/json'},
+        });
+      });
 
     return new Response(
       JSON.stringify({
